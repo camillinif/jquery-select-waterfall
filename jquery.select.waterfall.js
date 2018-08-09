db1cd84bf85a4aa1371c41ca84f3244cf3a12e6c
Francesco Camillini <francesco.camillini@gmail.com>
2018-08-09 08:30:30 +0200

    adding data origin callback

--- jquery.select.waterfall.js
+++ jquery.select.waterfall.js
@@ -4,15 +4,34 @@ $(document).ready(function(){
 
 (function ($){
     $.fn.initWaterfall = function(){
-        $('select[data-waterfall]').on('change', null, 'root', this.updateChild);
+
+    $('select[data-waterfall]').on('change', null, 'root', this.updateChild);
     }
     $.fn.updateChild = function(event){
         if(event.data !== 'root') {
             $(this).find('option:not(:first)').remove();
-        }
+            dataOrigin = $(this).data('origin');
+
+            var a = eval(dataOrigin);
+            var dfd = jQuery.Deferred();
+
+            dfd.promise(a);
+            a.done(function(data){
+                console.log(data);
+            });
+        } 
+
+
 
         var childSelectArray = $('select[data-parent="#'+$(this).attr('id')+'"]');
         childSelectArray.each($(this).updateChild);
     }
 }(jQuery));
 
+function test() {
+    return [3,2,1];
+}
+
+function test2() {
+    return [3,4,5];
+}
