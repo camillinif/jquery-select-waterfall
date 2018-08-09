aed05021b9faf20e924ebc80834b38bee34fa5dc
Francesco Camillini <francesco.camillini@gmail.com>
2018-08-09 16:42:34 +0200

    update repository structure

--- jquery.select.waterfall.js
+++ jquery.select.waterfall.js
@@ -86,21 +86,3 @@ $(document).ready(function(){
     }
 
 }(jQuery));
-
-function test() {
-    return $.ajax({
-        url: 'https://jsonplaceholder.typicode.com/todos'
-    }).then(function(data){
-        return {
-            data: data
-        }
-    });
-}
-var loadingStyle = function (element) {
-    $(element).html('carico');
-}
-
-var normalStyle = function (element) {
-    console.log(1);
-    $(element).html('');
-}
